a12f0c62a7dc058e0972701c38a14da7a5f72bd8
KITCOOP <KITCOOP@DESKTOP-BJ2RMQC>
2020-07-03 17:49:52 +0900

    2020-07-03 수정

--- Django_final_project/ks/views.py
+++ Django_final_project/ks/views.py
@@ -8,6 +8,8 @@ import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import to_categorical
+import plotly.express as px
+from plotly.offline import plot
 plt.rc('font', family='malgun gothic')
 plt.rcParams['axes.unicode_minus'] = False
 
@@ -18,6 +20,7 @@ def mainFunc(request):
     data = pd.read_csv('https://raw.githubusercontent.com/pyh3887/Django_final_project/master/student.csv', encoding='euc-kr')
 # '성별', '국적', '출생지', '교육단계', '학년', '학급', '전공', '학기', '담당부모', '발표수', '과정반복수', '새공지사항확인수', '토론참여수', '부모의학교만족도', '결석일수', '성적'
     df = data
+    print(data.columns)
     # LabelEncoder
     le = LabelEncoder()
     # apply "le.fit_transform"
@@ -41,34 +44,42 @@ def mainFunc(request):
 #     값이 +0.3 ~ +0.7 이면, 뚜렷한 양적 상관관계
 #     값이 +0.7 ~ +1.0 이면, 강한 양적 상관관계로 해석됩니다.
 
-    plt.figure(figsize=(9, 9))
-    sns.heatmap(df.corr(), annot=True, fmt = '.2f', linewidths=.5, cmap='RdBu_r')
-    fig = plt.gcf()
-    fig.savefig(os.path.dirname(os.path.realpath(__file__)) + '\\static\\images\\heatmap.png')
-    plt.clf()
-    # 발표 수와 상관관계의 값이 0.3 이상인 것은 담당부모, 과정반복수, 새공지사항확인수, 토론참여수, 부모의학교만족도, 결석일수가 있다.
+#     plt.figure(figsize=(9, 9))
+#     sns.heatmap(df.corr(), annot=True, fmt = '.2f', linewidths=.5, cmap='RdBu_r')
+#     fig = plt.gcf()
+#     fig.savefig(os.path.dirname(os.path.realpath(__file__)) + '\\static\\images\\heatmap.png')
+#     plt.clf()
+
+    fig = px.imshow(df.corr(),
+                    x=['성별', '국적', '출생지', '교육단계', '학년', '학급', '전공', '학기', '담당부모', '발표수', '과정반복수', '새공지사항확인수', '토론참여수', '부모의학교만족도', '결석일수', '성적'],
+                    y=['성별', '국적', '출생지', '교육단계', '학년', '학급', '전공', '학기', '담당부모', '발표수', '과정반복수', '새공지사항확인수', '토론참여수', '부모의학교만족도', '결석일수', '성적'],
+                    width=900, height=900, color_continuous_scale='RdBu_r'
+               )
+    plot_div = plot(fig,output_type='div')
+    # 발표 수와 상관관계의 값이 0.3 이상인 것은 담당부모, 과정반복수, 새공지사항확인수, 토론참여수, 부모의학교만족도, 결석일수, 성적이 있다.
     # 그러면 이 칼럼들로 발표수를 예측해 보겠다.
     print(df['담당부모'])
-    has = df.iloc[:, 9] + df.iloc[:, 11:15]
-#     print(has)
-#     xdata = np.random.random((1000, 12))
-#     ydata = np.random.randint(10, size=(1000, 1))
-#     ydata = to_categorical(ydata, num_classes=10)
-#     print(xdata[:2], xdata.shape)
-#     print(ydata[:2], ydata.shape)
-#     
-#     model = Sequential()
-#     model.add(Dense(100, input_shape=(12, ), activation='relu'))
-#     model.add(Dense(50, activation='relu'))
-#     model.add(Dense(10, activation='softmax'))
-#     print(model.summary())
-#     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
-#     
-#     hist = model.fit(xdata, ydata, epochs=500, batch_size=32, verbose=2)
-#     model_eval = model.evaluate(xdata, ydata)
-#     print('model_eval :', model_eval)
-#     
-#     print('예측 값 :', [np.argmax(i) for i in (model.predict(xdata[:5]))])     # 가장 큰 값을 찾기 위해서 np.argmax를 사용한다.
-#     print('실제 값 :', [np.argmax(i) for i in ydata[:5]])
+    xhas = df.loc[:, ['발표수', '과정반복수', '새공지사항확인수', '토론참여수', '부모의학교만족도', '결석일수', '성적']]
+#     xhas = df.loc[:, ['담당부모', '새공지사항확인수', '토론참여수', '부모의학교만족도', '결석일수', '성적']]
+    yhas = df.loc[:, ['담당부모']]
     
-    return render(request,'aa.html')
+    xdata = xhas.values
+    ydata = yhas.values
+    print(xdata[:2], xdata.shape)
+    print(ydata[:2], ydata.shape)
+    
+    model = Sequential()
+    model.add(Dense(64, input_shape=(7, ), activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(2, activation='linear'))
+    print(model.summary())
+    model.compile(optimizer='adam', loss='mse', metrics=['acc'])
+    
+    model.fit(xdata, ydata, epochs=100, batch_size=10, verbose=0)
+    model_eval = model.evaluate(xdata, ydata)
+    print('model_eval :', model_eval)
+    new_x = [[1, 80, 50, 100, 1, 10, 3]]
+    print('예측 값 :', model.predict(xdata))
+    print('실제 값 :', ydata)
+
+    return render(request,'aa.html', context={'plot_div': plot_div})
